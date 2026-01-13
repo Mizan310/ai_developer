c8b0d53950b73e0a5b70c50894478f37acbe577e
Your Name <youremail@example.com>
2026-01-13 11:51:52 +0600

    commiting last updates upto SQL

---
+++ sdsdf.session.sql
@@ -0,0 +1,16 @@
+-- insert into table1(name)
+-- values("Atik");
+
+-- SELECT * FROM table1
+
+-- DELETE FROM table1 WHERE `table1`.`id` = 8
+
+-- SELECT * FROM table1
+
+-- DELETE FROM table1 WHERE `table1`.`id` = 7
+
+SELECT * FROM table1
+
+
+
+
